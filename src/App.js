5f3c8fbdb30620079d85b3898defc76e1ae66ab7
SAGUN JAISWAL <63302439+sagunjaiswal@users.noreply.github.com>
2020-07-10 00:10:05 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
+import "./bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <SearchBar />
+    </>
+  );
+}
+
+export default App;
